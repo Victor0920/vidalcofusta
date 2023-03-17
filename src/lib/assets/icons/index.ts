b400eab169ea1c4f8fa28d4443be7e3475acf9b0
Victor0920 <vivi181016@gmail.com>
2023-03-17 06:57:29 +0100

    Added some icons and images

---
+++ src/lib/assets/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as ArrowheadRightIcon } from './arrowhead-right.svelte';
+export { default as Logo } from './logo.svelte';
+export { default as MenuIcon } from './menu.svelte';
---
+++ src/lib/assets/icons/menu.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let width: Number = 20;
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={`${width}`} height={`${width}`}
+	><rect width="24" height="24" transform="rotate(180 12 12)" opacity="0" /><rect
+		x="3"
+		y="11"
+		width="18"
+		height="2"
+		rx=".95"
+		ry=".95"
+	/><rect x="3" y="16" width="18" height="2" rx=".95" ry=".95" /><rect
+		x="3"
+		y="6"
+		width="18"
+		height="2"
+		rx=".95"
+		ry=".95"
+	/>
+</svg>
