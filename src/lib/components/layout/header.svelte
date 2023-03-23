f725a49e1dea0bf6069d4fd89c6c0bdab6cf84b1
Victor0920 <vivi181016@gmail.com>
2023-03-23 06:47:27 +0100

    Added clickoutside action and mobile dropdown to header

--- src/lib/components/layout/header.svelte
+++ src/lib/components/layout/header.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import { MenuIcon, Logo } from '$lib/assets/icons';
-	import { hasScrolledFromTop } from '$lib/actions/hasScrolledFromTop';
+	import { MenuIcon, Logo, CloseIcon } from '$lib/assets/icons';
+	import { hasScrolledFromTop, clickOutside } from '$lib/actions';
+	import { onMount } from 'svelte';
 
 	let hasScrolled = false;
+	let showDropdownMenu = false;
 
 	const navbarItems = [
 		{
@@ -18,26 +20,24 @@
 			url: '/contacto'
 		}
 	];
+
+	onMount(async () => {
+		hasScrolled = window?.scrollY > 0;
+	});
 </script>
 
 <div
 	class={`flex bg-transparent justify-center sticky top-0 items-center z-10 px-10 py-8 sm:p-5 lg:pr-16 transition-all duration-500 ${
 		hasScrolled ? 'shadow-lg' : ''
 	}`}
-	class:header-opaque={hasScrolled}
+	class:opaque-backdrop-filter={hasScrolled}
 	use:hasScrolledFromTop
-	on:hasScrolled={(e) => {
-		console.log(e.detail);
-		hasScrolled = e.detail;
-	}}
+	on:hasScrolled={(e) => (hasScrolled = e.detail)}
+	use:clickOutside
+	on:click_outside={() => (showDropdownMenu = false)}
 >
-	<!-- use:hasScrolledFromTop
-	on:hasScrolledFromTop={() => {
-		console.log('hey');
-	}} -->
-	<!-- shadow-lg -->
 	<div class="flex justify-between w-full max-w-[1000px]">
-		<a href="/">
+		<a href="/" on:click={() => (showDropdownMenu = false)}>
 			<Logo
 				iconColor={hasScrolled ? '#3080c2' : '#9fbcff'}
 				lettersColor={hasScrolled ? '#47476b' : '#ffffff'}
@@ -45,21 +45,42 @@
 		</a>
 		<nav class="flex sm:hidden gap-10">
 			{#each navbarItems as item}
-				<a class={`flex items-center ${hasScrolled ? 'text-black' : 'text-white'}`} href={item.url}
-					>{item.text}</a
-				>
+				<a class={`flex items-center ${hasScrolled ? 'text-black' : 'text-white'}`} href={item.url}>
+					{item.text}
+				</a>
 			{/each}
 		</nav>
 		<div
 			class="hidden sm:block h-min rounded-full hover:bg-gray-300/50 p-2 cursor-pointer transition-all duration-800"
+			on:click={() => (showDropdownMenu = !showDropdownMenu)}
 		>
-			<MenuIcon color={hasScrolled ? '#000000' : '#ffffff'} />
+			{#if !showDropdownMenu}
+				<MenuIcon color={hasScrolled ? '#000000' : '#ffffff'} />
+			{:else}
+				<CloseIcon color={hasScrolled ? '#000000' : '#ffffff'} />
+			{/if}
 		</div>
 	</div>
+
+	{#if showDropdownMenu}
+		<nav class="absolute w-full bottom-0 left-0 translate-y-[100%] p-2">
+			<div class="w-full rounded-md bg-white flex flex-col overflow-hidden shadow-lg">
+				{#each navbarItems as item}
+					<a
+						class="p-5 text-black text-center border-t-2 first:border-t-0 border-gray-100"
+						href={item.url}
+						on:click={() => (showDropdownMenu = false)}
+					>
+						{item.text}
+					</a>
+				{/each}
+			</div>
+		</nav>
+	{/if}
 </div>
 
 <style>
-	.header-opaque {
+	.opaque-backdrop-filter {
 		backdrop-filter: saturate(180%) blur(5px);
 		background-color: hsla(0, 0%, 100%, 0.8);
 	}
