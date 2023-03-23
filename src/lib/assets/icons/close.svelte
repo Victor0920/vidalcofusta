33377cc7bf592d96085aad80f67fd42b4cf6c629
Victor0920 <vivi181016@gmail.com>
2023-03-23 06:48:36 +0100

    Added icons and images

---
+++ src/lib/assets/icons/close.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let width: Number = 20;
+	export let color: string = '#000000';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width={`${width}`} height={`${width}`}>
+	<path
+		stroke={color}
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-miterlimit="10"
+		d="M7.2,7L23,23.1 M7.2,23L23,7"
+	/>
+</svg>
