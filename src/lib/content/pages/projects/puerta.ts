9b050b50a802f6a30dcd244e50128f6c9c6922ef
Victor0920 <vivi181016@gmail.com>
2023-03-25 18:04:59 +0100

    Updated Puerta project content and added images

--- src/lib/content/pages/projects/puerta.ts
+++ src/lib/content/pages/projects/puerta.ts
@@ -1,16 +1,21 @@
 import type { PageType } from '$lib/types';
+
+// Termporal. Las imágenes irán en un repo externo
 import puertaImg from '$lib/assets/puerta.jpeg';
+import puertaImg2 from '$lib/assets/puerta2.png';
+import puertaImg3 from '$lib/assets/puerta3.png';
+import puertaIcon from '$lib/assets/puerta_icon.png';
 
 const puerta: PageType = {
 	id: 'p3',
 	name: 'puerta',
 	url: '/galeria/puerta',
 	meta: {
-		title: 'Puerta de madera',
-		description:
-			'Puerta a base de palets. Fabricada Únicamente de madera, incluídas las bisagras. Sin un solo tornillo.',
+		title: 'Portón de entrada',
+		description: 'Puerta triple para la entrada de personas y vehículos.',
 		imageUrl: puertaImg,
-		imageDescription: 'Puerta de entrada de palets de madera'
+		imageDescription: 'Puerta de entrada de palets de madera',
+		imageUrlReduced: puertaIcon
 	},
 	sections: [
 		{
@@ -18,8 +23,8 @@ const puerta: PageType = {
 			background: '#ffffff',
 			content: {
 				title: {
-					type: 'h1',
-					text: 'Reclicablidad y reusabilidad para maderas que podrían ser cenizas'
+					size: 'medium',
+					text: '90% materiales reciclados, 100% innovación'
 				},
 				paragraphs: [
 					'Cillum veniam est culpa adipisicing ullamco excepteur fugiat. Occaecat do ipsum cupidatat proident nisi deserunt qui quis in aliqua.',
@@ -32,8 +37,40 @@ const puerta: PageType = {
 			type: 'ImageAndText',
 			background: '#f3f1e9',
 			content: {
-				image: 'imgUrl',
-				texts: ['first text', 'second text']
+				image: puertaImg3,
+				imageDescription: 'Estructura de la puerta',
+				imagePosition: 'start',
+				text: {
+					title: {
+						size: 'medium',
+						text: 'Estructura'
+					},
+					paragraphs: [
+						'Elit sunt cupidatat dolore pariatur incididunt eiusmod consequat nostrud nulla sunt irure magna id.',
+						'Sit ut non Lorem laboris enim culpa magna ipsum. Magna cillum esse aute amet occaecat. Quis fugiat sunt dolore irure sit minim non proident exercitation incididunt esse sint non ipsum.'
+					],
+					textColor: '#000000'
+				}
+			}
+		},
+		{
+			type: 'ImageAndText',
+			background: '#ffffff',
+			content: {
+				image: puertaImg2,
+				imageDescription: 'some image description',
+				imagePosition: 'end',
+				text: {
+					title: {
+						size: 'medium',
+						text: 'Bisagras'
+					},
+					paragraphs: [
+						'Elit sunt cupidatat dolore pariatur incididunt eiusmod consequat nostrud nulla sunt irure magna id.',
+						'Sit ut non Lorem laboris enim culpa magna ipsum. Magna cillum esse aute amet occaecat. Quis fugiat sunt dolore irure sit minim non proident exercitation incididunt esse sint non ipsum.'
+					],
+					textColor: '#000000'
+				}
 			}
 		},
 		{
