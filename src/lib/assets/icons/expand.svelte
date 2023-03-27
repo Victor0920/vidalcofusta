3765c377e0c10a8bbd4655bf7fa7318f51294c67
Victor0920 <vivi181016@gmail.com>
2023-03-27 07:30:48 +0200

    Added icons

---
+++ src/lib/assets/icons/expand.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let width: Number = 20;
+	export let color: string = '#000000';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={`${width}`} height={`${width}`}>
+	<rect width="24" height="24" transform="rotate(180 12 12)" opacity="0" />
+	<path
+		fill={color}
+		d="M20 5a1 1 0 0 0-1-1h-5a1 1 0 0 0 0 2h2.57l-3.28 3.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L18 7.42V10a1 1 0 0 0 1 1 1 1 0 0 0 1-1z"
+	/>
+	<path
+		fill={color}
+		d="M10.71 13.29a1 1 0 0 0-1.42 0L6 16.57V14a1 1 0 0 0-1-1 1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h5a1 1 0 0 0 0-2H7.42l3.29-3.29a1 1 0 0 0 0-1.42z"
+	/>
+</svg>
--- src/lib/assets/icons/index.ts
+++ src/lib/assets/icons/index.ts
@@ -1,4 +1,6 @@
+export { default as ArrowIcon } from './arrow.svelte';
 export { default as ArrowheadRightIcon } from './arrowhead-right.svelte';
 export { default as CloseIcon } from './close.svelte';
+export { default as ExpandIcon } from './expand.svelte';
 export { default as Logo } from './logo.svelte';
 export { default as MenuIcon } from './menu.svelte';
