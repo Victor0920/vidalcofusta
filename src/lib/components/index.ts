0cb32aab50b244e704a413b152eb912b8d7710f0
Victor0920 <vivi181016@gmail.com>
2023-03-27 07:30:34 +0200

    Moved main layout components into main folder (inside layout)

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,10 +1,14 @@
-export { default as Footer } from './layout/footer.svelte';
-export { default as Header } from './layout/header.svelte';
-export { default as PageIntro } from './layout/pageIntro.svelte';
-export { default as PageSection } from './layout/pageSection.svelte';
+export { default as Footer } from './layout/main/footer.svelte';
+export { default as Header } from './layout/main/header.svelte';
+export { default as PageIntro } from './layout/main/pageIntro.svelte';
+export { default as PageSection } from './layout/main/pageSection.svelte';
+
+export { default as ColumnLayout2 } from './layout/columnLayout2.svelte';
 
 export { default as Card } from './sections/card.svelte';
 export { default as Cards } from './sections/cards.svelte';
 export { default as Text } from './sections/text.svelte';
+export { default as ImageAndText } from './sections/imageAndText.svelte';
+export { default as ImageCarrousel } from './sections/imageCarrousel.svelte';
 
 export { default as Section } from './section.svelte';
