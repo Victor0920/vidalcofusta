d34c3ad5539c222c55940f7400b2f7fb973c0e0f
Victor0920 <vivi181016@gmail.com>
2023-03-29 06:34:03 +0200

    Added pageSection pt and pb props

--- src/lib/components/layout/main/pageSection.svelte
+++ src/lib/components/layout/main/pageSection.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	export let background: string = '#ffffff';
+	export let pt: number = 16;
+	export let pb: number = 16;
 </script>
 
 <div
-	class="page-section w-full p-16 sm:px-5 sm:py-10 flex justify-center overflow-hidden"
+	class={`page-section w-full p-16 pt-${pt} pb-${pb} sm:px-5 sm:py-10 flex justify-center overflow-hidden`}
 	style:background
 >
 	<div class="w-full max-w-[900px] relative">
