36730d80e48a39c9c9c9a0797008cea865a769ac
Victor0920 <vivi181016@gmail.com>
2023-04-26 06:56:05 +0200

    Added new theme colors to button

--- src/lib/components/components/button.svelte
+++ src/lib/components/components/button.svelte
@@ -4,20 +4,53 @@
 	export let content: ButtonType;
 
 	let { text, theme, url } = content;
+
+	$: themeColor =
+		theme === 'red'
+			? '#ff0000'
+			: theme === 'blue'
+			? '#0081c6'
+			: theme === 'green'
+			? '#3FDC5A'
+			: theme === 'yellow'
+			? '#ffff00'
+			: '#bbbbbb';
 </script>
 
 <div class="w-full flex justify-center">
 	<a
-		class={` py-4 px-10 w-min shadow-lg rounded-xl overflow-hidden hover:scale-[101%] hover:shadow-xl transition-all duration-100 theme-${theme} font-bold`}
+		class="button py-4 px-10 w-max shadow-lg rounded-xl overflow-hidden hover:scale-[103%] hover:shadow-xl active:scale-[98%] active:shadow-lg transition-all duration-100 font-bold relative"
+		style="--theme-color: {themeColor}"
 		href={url}
 	>
-		{text}
+		<span class="button__text relative">
+			{text}
+		</span>
 	</a>
 </div>
 
-<style>
-	.theme-red {
-		background-color: #ff0000;
-		color: #ffffff;
+<style lang="scss">
+	.button {
+		color: var(--theme-color);
+		background-color: #ffffff;
+
+		&__text::before {
+			content: '';
+			position: absolute;
+			bottom: -19px;
+			left: -37px;
+			width: 200%;
+			height: 2px;
+			background-color: var(--theme-color);
+			transition: all 0.1s cubic-bezier(0.4, 0, 0.2, 1);
+		}
+		&:hover {
+			.button__text::before {
+				bottom: 0;
+				left: 0;
+				width: 100%;
+				height: 1px;
+			}
+		}
 	}
 </style>
